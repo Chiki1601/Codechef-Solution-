b1dff61b5c7af8b963564ebc120faf525674c7e5
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-19 11:05:02 +0530

    Create ZUBTRCNT.cpp

---
+++ Beginners/ZUBTRCNT.cpp
@@ -0,0 +1,24 @@
+//https://www.codechef.com/problems/ZUBTRCNT
+
+
+#include <iostream>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int t;
+	std::cin >> t;
+	for (int i = 1; i <= t; ++i) {
+		int l;
+		int k;
+		std::cin >> l >> k;
+		if (k > l) {
+			std::cout << "Case " << i << ": 0\n";
+		} else {
+			int ans = l - k + 1;
+			ans = ans * (ans + 1) / 2;
+			std::cout << "Case " << i << ": " << ans << "\n";
+		}
+	}
+	return 0;
+}
