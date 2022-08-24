403926fdcb8b6df8b388f853f66419b3a976f0c6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-24 12:10:22 +0530

    Create LUCKYSTR.cpp

---
+++ Easy/LUCKYSTR.cpp
@@ -0,0 +1,38 @@
+/*
+ * @author Pooja Anilkumar Patel (chiki1601)
+ * github: http://www.github.com/chiki1601
+ * Created on 24/08/22.
+ * Problem link: https://www.codechef.com/problems/LUCKYSTR
+ */
+
+#include<iostream>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int k, n;
+	std::cin >> k >> n;
+	std::string arr[k];
+	std::string str[n];
+	for (int i = 0; i < k; i++)
+		std::cin >> arr[i];
+	for (int i = 0; i < n; i++)
+		std::cin >> str[i];
+	for (int i = 0; i < n; i++) {
+		if (str[i].length() >= 47)
+			std::cout << "Good\n";
+		else {
+			bool flag = false;
+			for (int j = 0; j < k; j++)
+				if (str[i].find(arr[j]) != std::string::npos) {
+					flag = true;
+					break;
+				}
+			if (flag)
+				std::cout << "Good\n";
+			else
+				std::cout << "Bad\n";
+		}
+	}
+	return 0;
+}
