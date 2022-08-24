04e81a6baf57a247b3d8bf8bf94ae752224255d7
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-24 12:11:52 +0530

    Create MYSARA.cpp

---
+++ Easy/MYSARA.cpp
@@ -0,0 +1,44 @@
+/*
+ * @author Pooja Anilkumar Patel (chiki1601)
+ * github: http://www.github.com/chiki1601
+ * Created on 24/08/22.
+ * Problem link: https://www.codechef.com/problems/MYSARA
+ */
+
+#include <iostream>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int t;
+	std::cin >> t;
+	while (t--) {
+		int n;
+		std::cin >> n;
+		int arr[n];
+		for (int i = 0; i < n; ++i) {
+			std::cin >> arr[i];
+		}
+		bool flag = true;
+		for (int i = 0; i < n - 1; ++i) {
+			if ((arr[i] & arr[i + 1]) != arr[i]) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag) {
+			int cnt = 0;
+			for (int i = 0; i < n - 1; ++i) {
+				cnt += __builtin_popcount(arr[i]);
+			}
+			int ans = 1;
+			for (int i = 0; i < cnt; ++i) {
+				ans = ans * 2 % 1000000007;
+			}
+			std::cout << ans << "\n";
+		} else {
+			std::cout << "0\n";
+		}
+	}
+	return 0;
+}
