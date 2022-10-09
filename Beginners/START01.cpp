f8bf70a79c937255d1751775eb88eb61238af555
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-09 11:52:21 +0530

    Create START01.cpp

---
+++ Beginners/START01.cpp
@@ -0,0 +1,16 @@
+/*
+ * @author Miss Pooja Anilkumar Patel (chiki1601)
+ * github: http://www.github.com/chiki1601
+ * Created on 09/10/22.
+ * Problem link: https://www.codechef.com/problems/START01
+ */
+#include <iostream>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int n;
+	std::cin >> n;
+	std::cout << n;
+	return 0;
+}
