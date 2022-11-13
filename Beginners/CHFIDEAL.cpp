d3c172e921a6bbd5659f051b6a35a425fe934fa9
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-13 13:46:09 +0530

    Create CHFIDEAL.cpp

---
+++ Beginners/CHFIDEAL.cpp
@@ -0,0 +1,16 @@
+/*
+ * @author SMiss Pooja Anilkumar Patel (chiki1601)
+ * github: http://www.github.com/chiki1601
+ * Created on 13/11/22.
+ * Problem link: https://www.codechef.com/problems/CHFIDEAL
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "1" << std::endl;
+	int x;
+	std::cin >> x;
+	std::cout << (x ^ 1) << std::endl;
+	return 0;
+}
