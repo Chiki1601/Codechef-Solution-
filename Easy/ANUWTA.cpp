9cfb2f6608d52c7addb8234e2040b3a005d8d2ee
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-14 12:30:56 +0530

    Create ANUWTA.cpp

---
+++ Easy/ANUWTA.cpp
@@ -0,0 +1,26 @@
+/*
+ * @author Miss Pooja Anilkumar Patel (chiki1601)
+ * github: http://www.github.com/chiki1601
+ * Created on 8/1/20.
+ * Problem link: https://www.codechef.com/problems/ANUWTA
+ */
+
+#include <iostream>
+#include <array>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::array<long, 100001> arr{0};
+	for (int i = 1; i < arr.size(); ++i) {
+		arr[i] = i + 1 + arr[i - 1];
+	}
+	int t;
+	std::cin >> t;
+	while (t--) {
+		int n;
+		std::cin >> n;
+		std::cout << arr[n] << "\n";
+	}
+	return 0;
+}
